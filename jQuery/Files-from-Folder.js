9653bdd631bde82360a14ed1ef8a1e916acd3bd9
MaximumFX <MaxiumumFX@users.noreply.github.com>
2017-03-28 20:46:02 +0200

    Update Files-from-Folder.js

--- jQuery/Files-from-Folder.js
+++ jQuery/Files-from-Folder.js
@@ -1,9 +1,9 @@
-var dir = "URL";
-var fileextension="EXT";
+var dir = 'URL';
+var fileextension='EXT';
 $.ajax({
   url: dir,
   success: function (data) {
-    $(data).find("a:contains(" + fileextension + ")").each(function() {
+    $(data).find('a:contains(' + fileextension + ')').each(function() {
       var filename = this.href;
     });
   }
